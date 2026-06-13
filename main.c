f23a1bec5787be7b51cccc99a15d409fab1be04e
Vadzim Tsitko <vtitko27@gmail.com>
2026-06-13 19:02:39 +0200

    Learned a bit about structs

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct Vec2 {
+    float x; 
+    float y;
+} Vec2;
+
+typedef struct Ship {
+    Vec2 pos;
+    Vec2 vel;
+    float angle;
+    bool alive;
+} Ship;
+
+Vec2 vec2_add(Vec2 a, Vec2 b){
+    Vec2 res;
+    res.x = a.x + b.x;
+    res.y = a.y + b.y;
+
+    return res;
+}
+
+
+void vec2_add_in_palce(Vec2 *a, Vec2 b){
+    (*a).x = (*a).x + b.x;
+    (*a).y = (*a).y + b.y;
+}
+
 
 int main(void){
-    printf("Hello World!\n");
+    Vec2 a = {5, 5};
+    Vec2 b = {7, 7};
+
+    Vec2 c = vec2_add(a, b);
+
+    printf("A = (X=%f, Y=%f)\n", a.x, a.y);
+    printf("B = (X=%f, Y=%f)\n", b.x, b.y);
+    printf("C = (X=%f, Y=%f)\n", c.x, c.y);
+
+    vec2_add_in_palce(&a, b);
+
+    printf("A = (X=%f, Y=%f)\n", a.x, a.y);
+
+    printf("sizeof(Vec2) = %zu\n", sizeof(Vec2));
+    printf("sizeof(Ship) = %zu\n", sizeof(Ship));
+
     return 0;
 }
