068639415735ba75c335c536f62f5d8ed2c1d2f6
Vadzim Tsitko <vtitko27@gmail.com>
2026-06-18 00:15:42 +0200

    Add the example

--- raylib_test.c
+++ raylib_test.c
@@ -3,6 +3,14 @@
 int main(void){
     InitWindow(800, 450, "raylib game flow testing");
 
+    while (!WindowShouldClose()){
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Hey mate!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
     CloseWindow();
 
     return 0;
