1f8666f15f572d6f41aec81e5efccd7883eeae35
Vadzim Tsitko <vtitko27@gmail.com>
2026-06-24 22:51:47 +0200

    Add basic acceleration to the ship

--- raylib_test.c
+++ raylib_test.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +16,12 @@ typedef struct Asteroid {
     Vec2 position;
     Vec2 velocity;
 } Asteroid;
+
+typedef struct Ship{
+    Vec2 position;
+    Vec2 velocity;
+    float angle;
+} Ship;
 
 
 float random_float(float low, float high){
@@ -61,10 +68,33 @@ void update(Vec2 *position, Vec2 *velocity, float dt, int high, int radius) {
 }
 
 
+void change_speed(Vec2 *velocity, float alpha, float ceiling){
+    float speed = sqrtf(velocity->x * velocity->x + velocity->y * velocity->y);
+    printf("Speed: %f\n", speed);
+    
+    if (speed < ceiling || alpha < 0){
+        velocity->x = velocity->x + alpha;
+        velocity->y = velocity->y + alpha;
+    }
+
+    if (velocity->x < 0){
+        velocity->x = 0;
+    }
+
+    if (velocity->y < 0){
+        velocity->y = 0;
+    }
+}
+
+
 void calculate_next_asteroids_coordinates(Asteroid asteroids[], int number, int radius, float dt, int high){
     for (int i = 0; i < number; i++){
         update(&(asteroids[i].position), &(asteroids[i].velocity), dt, high, radius);
     }
+}
+
+void calculate_next_ship_coordinates(Ship *ship, int radius, float dt, int high){
+    update(&ship->position, &ship->velocity, dt, high, radius);
 }
 
 void draw_asteroids(Asteroid asteroids[], int number, int radius){
@@ -73,12 +103,35 @@ void draw_asteroids(Asteroid asteroids[], int number, int radius){
     }
 }
 
+void draw_ship(Ship ship, int radius){
+    DrawCircle(ship.position.x, ship.position.y, radius, GREEN);
+}
+
 
 int main(void){
     int low = 0;
     int hight = 900;
     int number_of_asteroids = 5;
+
     int hitbox_radius = 30;
+    int ship_radius = 10;
+
+    float max_speed = 50.0f;
+    float acceleration = 0.1f;
+    float deceleration = -0.2f;
+
+    Vec2 start_ship_position = {
+        .x=450,
+        .y=450
+    };
+    Vec2 start_ship_velocity = {
+        .x=0,
+        .y=0
+    };
+    Ship ship = {
+        .position=start_ship_position,
+        .velocity=start_ship_velocity,
+    };
 
     InitWindow(hight, hight, "raylib game flow testing");
     SetTargetFPS(60);
@@ -89,11 +142,19 @@ int main(void){
     while (!WindowShouldClose()){
         float dt = (float)GetFrameTime();
         calculate_next_asteroids_coordinates(asteroids, number_of_asteroids, hitbox_radius, dt, hight);
+        calculate_next_ship_coordinates(&ship, ship_radius, dt, hight);
+
+        if (IsKeyDown(KEY_UP)){
+            change_speed(&ship.velocity, acceleration, max_speed);
+        } else {
+            change_speed(&ship.velocity, deceleration, max_speed);
+        }
 
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
             draw_asteroids(asteroids, number_of_asteroids, hitbox_radius);
+            draw_ship(ship, ship_radius);
 
         EndDrawing();
     }
